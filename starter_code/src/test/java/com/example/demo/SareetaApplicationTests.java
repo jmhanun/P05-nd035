bd4ca888dbbc3ca1f54e8d90f3320484159c2912
Jose Miguel Hanun <jmhanun@gmail.com>
2019-11-11 10:10:47 -0300

    Correcciones para que funcione jenkins

--- starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
+++ starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -6,9 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
 
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@EnableJpaRepositories
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@EnableJpaRepositories
 public class SareetaApplicationTests {
 
 	@Test
